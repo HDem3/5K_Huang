c2fb9475d19fa2cda58f2d9f132aa4eef00e3ba4
HDem3 <93908678+HDem3@users.noreply.github.com>
2022-02-09 00:59:21 +0100

    Update file

--- Es2_Fatture.php
+++ Es2_Fatture.php
@@ -4,19 +4,19 @@
 <?php
 	$Fatture = array (
 		array("Cliente"=>Marco,"Importo"=>52.45,"Pagato"=>"NO"),
-        array("Cliente"=>Andrea,"Importo"=>100.25,"Pagato"=>"SOSPESA"),
-        array("Cliente"=>Luca,"Importo"=>13.14,"Pagato"=>"NO")
+        	array("Cliente"=>Andrea,"Importo"=>100.25,"Pagato"=>"SOSPESA"),
+        	array("Cliente"=>Luca,"Importo"=>13.14,"Pagato"=>"NO")
 	);
     
-    $TotPag=0;
+    	$TotPag=0;
 	
-    for ($x = 0; $x <count($Fatture); $x++) {
+    	for ($x = 0; $x <count($Fatture); $x++) {
 		if($Fatture[$x]["Pagato"]=="NO"){
-        $TotPag = $TotPag + $Fatture[$x]["Importo"];	
-        echo $Fatture[$x]["Cliente"]."<br>Importo da pagare: ".$Fatture[$x]["Importo"]." euro<br>Stato Pagamento: ".$Fatture[$x]["Pagato"]."<br><br>";
-        };
+        		$TotPag = $TotPag + $Fatture[$x]["Importo"];	
+        		echo $Fatture[$x]["Cliente"]."<br>Importo da pagare: ".$Fatture[$x]["Importo"]." euro<br>Stato Pagamento: ".$Fatture[$x]["Pagato"]."<br><br>";
+        	};
 	};
-    echo "<br>Totale da pagare: ".$TotPag." euro";
+    	echo "<br>Totale da pagare: ".$TotPag." euro";
 ?>
 </body>
 </html>
