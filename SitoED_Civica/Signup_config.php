0abc09403d5a28a910e54816f2b7ae03eed9699a
HDem3 <93908678+HDem3@users.noreply.github.com>
2022-05-14 16:25:03 +0200

    Update Signup_config.php

--- SitoED_Civica/Signup_config.php
+++ SitoED_Civica/Signup_config.php
@@ -1,5 +1,5 @@
 <?php
-//my dates
+//my database
  $my_host = "localhost";
  $my_account = "huang4k";
  $my_password= "";
@@ -12,17 +12,60 @@
  }
  
  // get dates from html form
- $email=strtolower($_POST["Email"]);
+ //$email=strtolower($_POST["Email"]);
+ $email=filter_var($_POST['Email'], FILTER_SANITIZE_EMAIL);
  $username=strtolower($_POST["Username"]);
  $password= password_hash($_POST["password"], PASSWORD_DEFAULT);
  
- // comand SQL
- $query = "INSERT INTO My_Users (Email, Username, password) VALUES ('$email','$username','$password')";
+ $code= $_POST["code"];
+ if(isset($code) && !empty($code)){
+ 	$query = "SELECT * 
+ 		      FROM My_Users 
+              WHERE Verify_Code>'1111'";
+ 	$query_result= $connect->query($query);
+ 	$n_rows = $query_result->num_rows;
+ 	if ($n_rows==1){
+ 		$row= $query_result->fetch_assoc();
+        $verify_code=$row["Verify_Code"];
+        }
+    if($verify_code==$code){
+    	$query = "UPDATE My_Users
+		   		  SET Verify_Code = '0'
+		   		  WHERE Email = '$email' ;";
+ 		$connect-> query($query);
+    	header("Location: Login.html ");
+    	
+    }  
+ }else{
+
+ //email setting
+ $verify_code=rand(1111,8999);
+ $object="Verify your email";
+ $messagge='
+ <html>
+    <head>
+      <title>Verify your email</title>
+    </head>
+    <body>
+      <h1>Welcome to my site</h1>
+      <p>Please verify your email, this is your code:</p>
+        <h3>'.$verify_code.'</h3>
+      <p>Thanks, have a nice day :)</p>
+    </body>
+  </html>';
+  
+ //send verify code via email
+ $headers[] = 'MIME-Version: 1.0';
+ $headers[] = 'Content-type: text/html; charset=utf-8';
+ $verify_email= mail($email,$object,$messagge,implode("\r\n", $headers));
+ 
+ //inserisci dati in database
+ $query = "INSERT INTO My_Users (Email, Username, password,Verify_Code) VALUES ('$email','$username','$password','$verify_code')";
  if ($connect-> query($query)){
- 	header("Location: Login.html ");
+ 	header("Location: Verify_Email.html ");
  	}else {
  	echo "Error, with registration ". $connect->error. "\n";
 	}
+ }
  $connect->close();
- ?>
-
+?>
